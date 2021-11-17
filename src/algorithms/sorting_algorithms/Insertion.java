1880fd6f4987cad3e56fce604dbfb087dee2ceb1
shubham <shubham22121@gmail.com>
2021-11-17 23:17:50 +0530

    bubble

---
+++ src/algorithms/sorting_algorithms/Insertion.java
@@ -0,0 +1,9 @@
+package algorithms.sorting_algorithms;
+
+public class Insertion {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1,};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
