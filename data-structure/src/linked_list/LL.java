a6d6825b01bdd65d25087a72470e1b48736e6ca1
shubham <shubham22121@gmail.com>
2021-11-19 15:50:40 +0530

    ll

---
+++ data-structure/src/linked_list/LL.java
@@ -0,0 +1,27 @@
+package linked_list;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL(){
+        this.size = 0;
+    }
+
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
