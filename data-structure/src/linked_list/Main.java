77ba254b8cc8c64280ab0f0e3f6b97ab964838f0
shubham <shubham22121@gmail.com>
2021-11-19 14:57:51 +0530

    inbuil ll

---
+++ data-structure/src/linked_list/Main.java
@@ -0,0 +1,13 @@
+package linked_list;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(43);
+        list.add(7);
+        list.add(90);
+        System.out.println(list);
+    }
+}
