3c7d4f297ec01a526de98cfd998cb1a0d67bec57
shubham <shubham22121@gmail.com>
2021-11-19 22:15:03 +0530

    dll

--- data-structure/src/linked_list/Main.java
+++ data-structure/src/linked_list/Main.java
@@ -4,21 +4,33 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+
+//        LL list = new LL();
+//        list.insertFirst(8);
+//        list.insertFirst(32);
+//        list.insertFirst(5);
+//        list.insertFirst(92);
+//        list.insertLast(9);
+//        list.insertLast(3);
+//        list.insertLast(87);
+//        list.insert(22,3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+
+        DLL list = new DLL();
+        list.insertFirst(3);
+        list.insertFirst(2);
         list.insertFirst(8);
-        list.insertFirst(32);
-        list.insertFirst(5);
-        list.insertFirst(92);
-        list.insertLast(9);
-        list.insertLast(3);
-        list.insertLast(87);
-        list.insert(22,3);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(8, 65);
+
         list.display();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(2));
-        list.display();
+
     }
 }
