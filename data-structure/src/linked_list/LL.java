3f0d1b7b3cbc8d6f25196a0450656698bac2c542
shubham <shubham22121@gmail.com>
2021-11-19 17:40:23 +0530

    delete

--- data-structure/src/linked_list/LL.java
+++ data-structure/src/linked_list/LL.java
@@ -6,54 +6,104 @@ public class LL {
     private Node tail;
     private int size;
 
-    public LL(){
+    public LL() {
         this.size = 0;
     }
 
-    public void insertFirst(int val){
+    public void insertFirst(int val) {
         Node node = new Node(val);
         node.next = head;
         head = node;
 
-        if (tail==null){
+        if (tail == null) {
             tail = head;
         }
         size += 1;
     }
-    public void insertLast(int val){
-        if (tail == null){
+
+    public void insertLast(int val) {
+        if (tail == null) {
             insertFirst(val);
             return;
         }
 
         Node node = new Node(val);
-        tail.next= node;
+        tail.next = node;
         tail = node;
         size++;
     }
 
-    public void insert(int val, int index){
-        if (index == 0){
+    public void insert(int val, int index) {
+        if (index == 0) {
             insertFirst(val);
             return;
         }
-        if (index == size){
+        if (index == size) {
             insertLast(val);
             return;
         }
         Node temp = head;
-        for (int i = 1; i < index ; i++) {
+        for (int i = 1; i < index; i++) {
             temp = temp.next;
         }
-        Node node = new Node(val,temp.next);
+        Node node = new Node(val, temp.next);
         temp.next = node;
         size++;
-        
+
     }
 
-    public void display(){
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        return val;
+    }
+
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+
+    public void display() {
         Node temp = head;
-        while (temp!= null){
+        while (temp != null) {
             System.out.print(temp.value + "->");
             temp = temp.next;
 
@@ -75,7 +125,6 @@ public class LL {
             this.next = next;
         }
     }
-
 
 
 //    public static void main(String[] args) {
--- data-structure/src/linked_list/Main.java
+++ data-structure/src/linked_list/Main.java
@@ -9,15 +9,16 @@ public class Main {
         list.insertFirst(32);
         list.insertFirst(5);
         list.insertFirst(92);
-
-
         list.insertLast(9);
         list.insertLast(3);
         list.insertLast(87);
-
         list.insert(22,3);
-
         list.display();
-
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
     }
 }
