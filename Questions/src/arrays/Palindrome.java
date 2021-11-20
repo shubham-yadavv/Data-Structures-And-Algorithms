5e6351fab06e08afec525b0da5cd47e6f69994b0
shubham <shubham22121@gmail.com>
2021-11-20 22:28:19 +0530

    pallindrome

---
+++ Questions/src/arrays/Palindrome.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        String rev = "";
+
+        int len = str.length();
+
+        for (int i = len-1; i >=0 ; i--) {
+            rev = rev + str.charAt(i);
+
+        }
+        if (str.equals(rev)){
+            System.out.println("pallindrome");
+        }
+        else{
+            System.out.println("not pallindrome");
+        }
+    }
+}
