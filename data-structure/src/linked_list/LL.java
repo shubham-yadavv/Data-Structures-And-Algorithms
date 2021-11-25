19fb72321cd4c21c5842ad71baf4bacde0eaecc7
shubham <shubham22121@gmail.com>
2021-11-26 01:16:23 +0530

    liked list

--- data-structure/src/linked_list/LL.java
+++ data-structure/src/linked_list/LL.java
@@ -138,21 +138,6 @@ public class LL {
     }
 
 
-//    public static void main(String[] args) {
-//        LL list = new LL();
-//        list.insertFirst(8);
-//        list.insertFirst(32);
-//        list.insertFirst(5);
-//        list.insertFirst(92);
-//
-//
-//        list.insertLast(9);
-//        list.insertLast(3);
-//        list.insertLast(87);
-//
-//        list.display();
-//
-//    }
 }
 
 
