91de123dc5cf566cae2483e7fa4ecf7d95e04fb7
shubham <shubham22121@gmail.com>
2021-11-27 22:54:12 +0530

    public int[] buildArray(int[] nums) {

--- Questions/src/arrays/BuildArrayfromPermutation.java
+++ Questions/src/arrays/BuildArrayfromPermutation.java
@@ -3,9 +3,10 @@ package arrays;
 public class BuildArrayfromPermutation {
     public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
-        for (int i = 0; i < ans.length; i++){
-            ans[i]=nums[nums[i]];
+        for(int i = 0; i<nums.length; i++){
+            ans[i] = nums[nums[i]];
         }
         return ans;
+
     }
 }
