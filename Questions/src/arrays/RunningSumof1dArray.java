40bb91898273bdb004ca746022331892acb4d8a7
shubham <shubham22121@gmail.com>
2021-11-27 22:44:36 +0530

    RunningSumof1dArray

---
+++ Questions/src/arrays/RunningSumof1dArray.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class RunningSumof1dArray {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++){
+
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+
+    }
+}
