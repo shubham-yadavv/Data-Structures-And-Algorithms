bfe7935c11b15b77d460696289a04bddf03a21f4
shubham <shubham22121@gmail.com>
2021-11-28 13:34:52 +0530

    Running Sum of 1d Array

--- Questions/src/arrays/RunningSumof1dArray.java
+++ Questions/src/arrays/RunningSumof1dArray.java
@@ -3,7 +3,6 @@ package arrays;
 public class RunningSumof1dArray {
     public int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++){
-
             nums[i]+=nums[i-1];
         }
         return nums;
