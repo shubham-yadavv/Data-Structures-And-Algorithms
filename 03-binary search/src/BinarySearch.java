c1853977bc66f03af4759f6bf707a7c5cc76426c
shubham <shubham22121@gmail.com>
2021-12-14 17:41:52 +0530

    bimary search

---
+++ 03-binary search/src/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
+        int ans = binarysearch(arr, target);
+        System.out.println(ans);
+
+
+
+    }
+
+    static int binarysearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end- start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
+
+            }
+            if (arr[mid] > target) {
+                end = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+}
