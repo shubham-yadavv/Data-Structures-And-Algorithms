c28d96dfb2d6cf7bd8fb6a0f2e35a00edd6775eb
shubham <shubham22121@gmail.com>
2021-12-14 11:38:19 +0530

    string to array

--- linear-search/src/SearchInStrings.java
+++ linear-search/src/SearchInStrings.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class SearchInStrings {
     public static void main(String[] args) {
         String str = "shubham";
         System.out.println(search(str, 'a'));
+        System.out.println(Arrays.toString(str.toCharArray()));
+        System.out.println(search2(str, 'a'));
+    }
+
+
+    static boolean search2(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
+                return true;
+            }
+        }
+        return false;
     }
 
     static boolean search(String str, char target) {
