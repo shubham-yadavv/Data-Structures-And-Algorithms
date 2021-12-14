a41373731a3a64b97161729cf9ed39a9c20f795d
shubham <shubham22121@gmail.com>
2021-12-14 11:34:13 +0530

    string

---
+++ linear-search/src/SearchInStrings.java
@@ -0,0 +1,21 @@
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String str = "shubham";
+        System.out.println(search(str, 'a'));
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+}
