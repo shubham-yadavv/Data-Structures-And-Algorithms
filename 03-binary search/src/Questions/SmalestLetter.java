5dccbdbfdfbf822b6e767e1d4838e274d2d5eb28
shubham <shubham22121@gmail.com>
2021-12-15 23:33:11 +0530

    smallest num

--- 03-binary search/src/Questions/SmalestLetter.java
+++ 03-binary search/src/Questions/SmalestLetter.java
@@ -5,39 +5,25 @@ package Questions;
 public class SmalestLetter {
 
     public static void main(String[] args) {
-        char[] arr = {'c','f','j'};
-        int target = 'c';
-        int ans = nextGreatestLetter(arr, (char) target);
-        System.out.println(ans);
-
-
 
     }
 
-    static char nextGreatestLetter(char[] arr, char target) {
+    public char nextGreatestLetter(char[] letters, char target) {
 
+        int start = 0;
+        int end = letters.length - 1;
 
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
 
-
-            int start = 0;
-            int end = arr.length - 1;
-            while (start <= end) {
-                int mid = start + (end- start) / 2;
-
-                if (target < arr[mid]) {
-                    end = mid - 1;
-
-
-                }
-                else  {
-                    start = mid + 1;
-                }
-
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            return arr[start % arr.length];
-
-
+        }
+        return letters[start % letters.length];
     }
-
-
 }
