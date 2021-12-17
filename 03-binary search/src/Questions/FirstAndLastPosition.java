e8449ef55c2ed63dfbd62a76094dfe8f3c8cd359
shubham <shubham22121@gmail.com>
2021-12-17 23:25:04 +0530

    reformat

--- 03-binary search/src/Questions/FirstAndLastPosition.java
+++ 03-binary search/src/Questions/FirstAndLastPosition.java
@@ -17,29 +17,28 @@ public class FirstAndLastPosition {
         return ans;
     }
 
-     int search(int[] nums, int target, boolean firstIndex) {
+    int search(int[] nums, int target, boolean firstIndex) {
         int ans = -1;
-         int start = 0;
-         int end = nums.length - 1;
-         while (start <= end) {
-             int mid = start + (end - start) / 2;
-             if (target < nums[mid]) {
-                 end = mid - 1;
-             } else if (target > nums[mid]) {
-                 start = mid + 1;
-             } else {
-                 // potential ans found
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
                 ans = mid;
                 if (firstIndex) {
                     end = mid - 1;
-                }
-                else {
+                } else {
                     start = mid + 1;
                 }
-             }
-         }
+            }
+        }
 
-         return ans;
+        return ans;
     }
 
 }
