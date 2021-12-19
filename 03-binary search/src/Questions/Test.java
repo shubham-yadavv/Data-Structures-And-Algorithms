e0f76700de12375d65dc2334fc5269f68f842945
shubham <shubham22121@gmail.com>
2021-12-19 17:08:49 +0530

    linear search and binary search theory

--- 03-binary search/src/Questions/Test.java
+++ 03-binary search/src/Questions/Test.java
@@ -1,14 +1,12 @@
-//package Questions;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//        int[] arr = {2,3,5,9,14,16,18};
-////        int ans = test(arr);
-////        System.out.println(ans);
-//    }
-//
-//    static int binarysearch(int[] arr, int key) {
-//
-//
-//    }
-//}
+package Questions;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+
+        System.out.println(arr[7 % arr.length]);
+    }
+
+
+
+}
