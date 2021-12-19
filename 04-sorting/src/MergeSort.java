f12799532caf50d38f8236353c8f030063c4834c
shubham <shubham22121@gmail.com>
2021-12-19 20:59:53 +0530

    merge sort

---
+++ 04-sorting/src/MergeSort.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = Arrays.copyOfRange(arr, 0, mid);
+            int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+            mergeSort(left);
+            mergeSort(right);
+            merge(arr, left, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+}
+
---
+++ 04-sorting/src/README.md
@@ -0,0 +1,4 @@
+### Merge sort
+1. divide the array in 2 parts
+2. get both parts sorted 
+3. merge the two sorted parts. 
