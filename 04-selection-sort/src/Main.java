b0c9f2ef80d15f6501bde295c144091e4f41c932
shubham <shubham22121@gmail.com>
2021-12-19 20:06:45 +0530

    selection sort

---
+++ 04-selection-sort/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,3,2,4,1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+
+}
