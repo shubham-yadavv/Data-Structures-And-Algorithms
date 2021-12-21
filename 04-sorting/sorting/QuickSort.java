f154234375598b06c458130b2d035d93d7e9f73b
shubham <shubham22121@gmail.com>
2021-12-21 18:11:50 +0530

    stack

--- 04-sorting/sorting/QuickSort.java
+++ 04-sorting/sorting/QuickSort.java
@@ -2,61 +2,38 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = { 10, 7, 8, 9, 1, 5 };
-        int n = arr.length;
-        quickSort(arr, 0, n - 1);
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
+    static void sort(int[] nums, int low, int hi) {
 
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
 
-            // pi is partitioning index, arr[p]
-            // is now at right place
-            int pi = partition(arr, low, high);
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
 
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+        while (s <= e) {
 
-        }
-    }
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
 
-    static int partition(int[] arr, int low, int high)
-    {
-
-        // pivot
-        int pivot = arr[high];
-
-        // Index of smaller element and
-        // indicates the right position
-        // of pivot found so far
-        int i = (low - 1);
-
-        for(int j = low; j <= high - 1; j++)
-        {
-
-            // If current element is smaller
-            // than the pivot
-            if (arr[j] < pivot)
-            {
-
-                // Increment index of
-                // smaller element
-                i++;
-                swap(arr, i, j);
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
             }
         }
-        swap(arr, i + 1, high);
-        return (i + 1);
-    }
 
-    static void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        sort(nums, low, e);
+        sort(nums, s, hi);
     }
 }
---
+++ 08-stack/stack/Main.java
@@ -0,0 +1,69 @@
+
+class Stack {
+    int MAX = 1000;
+    int top;
+    int a[] = new int[MAX];
+
+
+    Stack(){
+        top = -1;
+    }
+
+    boolean push(int x) {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        else {
+            a[++top] = x;
+            System.out.println(x + " pushed into stack");
+            return true;
+        }
+    }
+
+    int pop() {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int top() {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            return x;
+        }
+    }
+
+    boolean isEmpty() {
+        return (top < 0);
+    }
+
+    void print(){
+        for(int i = top;i>-1;i--){
+            System.out.print(" "+ a[i]);
+        }
+    }
+}
+
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Top element is :" + s.top());
+        System.out.print("Elements present in stack :");
+        s.print();
+    }
+}
