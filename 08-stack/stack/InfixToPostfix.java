d90bdd99e582b30642c7377909af431b3ecaaaf0
shubham <shubham22121@gmail.com>
2021-12-26 13:59:56 +0530

    graphs

---
+++ 08-stack/stack/InfixToPostfix.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class InfixToPostfix {
+
+
+    static int precedence(char c){
+        switch (c){
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    static String infixToPostFix(String expression){
+
+        String postfix = "";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i <expression.length() ; i++) {
+            char c = expression.charAt(i);
+
+            //check if char is operator
+            if(precedence(c)>0){
+                while(stack.isEmpty()==false && precedence(stack.peek())>=precedence(c)){
+                    postfix += stack.pop();
+                }
+                stack.push(c);
+            }else if(c==')'){
+                char x = stack.pop();
+                while(x!='('){
+                    postfix += x;
+                    x = stack.pop();
+                }
+            }else if(c=='('){
+                stack.push(c);
+            }else{
+                //character is neither operator nor (
+                postfix += c;
+            }
+        }
+        for (int i = 0; i <=stack.size() ; i++) {
+            postfix += stack.pop();
+        }
+        return postfix;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter an expression");
+        Scanner sc = new Scanner(System.in);
+        String exp = sc.nextLine();
+
+        System.out.println("Infix Expression: " + exp);
+        System.out.println("Postfix Expression: " + infixToPostFix(exp));
+    }
+}
--- 08-stack/stack/Main.java
+++ 08-stack/stack/Main.java
@@ -1,5 +1,5 @@
 class Stack {
-    int MAX = 1000;
+    int MAX = 101;
     int top;
     int a[] = new int[MAX];
 
@@ -8,30 +8,26 @@ class Stack {
         top = -1;
     }
 
-    boolean push(int x) {
+    void push(int x) {
         if (top >= (MAX - 1)) {
             System.out.println("Stack Overflow");
-            return false;
         }
         else {
             a[++top] = x;
-            System.out.println(x + " pushed into stack");
-            return true;
+
+
         }
     }
 
-    int pop() {
-        if (top < 0) {
-            System.out.println("Stack Underflow");
-            return 0;
+    void pop() {
+        if(top == -1) {
+            System.out.println("Error: No element to pop");
+
         }
-        else {
-            int x = a[top--];
-            return x;
-        }
+        top--;
     }
 
-    int top() {
+    int Top() {
         if (top < 0) {
             System.out.println("Stack Underflow");
             return 0;
@@ -46,23 +42,33 @@ class Stack {
         return (top < 0);
     }
 
-    void print(){
-        for(int i = top;i>-1;i--){
+    void Print(){
+        for(int i = 0;i<=top;i++){
             System.out.print(" "+ a[i]);
         }
     }
+
 }
 
 class Main {
     public static void main(String args[])
     {
-        Stack s = new Stack();
-        s.push(10);
-        s.push(20);
-        s.push(30);
-        System.out.println(s.pop() + " Popped from stack");
-        System.out.println("Top element is :" + s.top());
-        System.out.print("Elements present in stack :");
-        s.print();
+        Stack S = new Stack();
+        S.push(2);S.Print();
+        System.out.println();
+
+        S.push(5);S.Print();
+        System.out.println();
+
+        S.push(10);S.Print();
+        System.out.println();
+
+        S.pop();S.Print();
+        System.out.println();
+
+        S.push(12);S.Print();
+        System.out.println();
+
+
     }
 }
