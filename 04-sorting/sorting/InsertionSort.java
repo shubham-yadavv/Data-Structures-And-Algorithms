e09a9ac52a8904546be4007a341f11b49d3dc90c
shubham <shubham22121@gmail.com>
2022-01-04 23:14:49 +0530

    insertion osrt

--- 04-sorting/sorting/InsertionSort.java
+++ 04-sorting/sorting/InsertionSort.java
@@ -2,21 +2,26 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {5, 2, 4, 6, 1, 3};
+        int[] arr = {5, 3, 4, 1, 2};
         insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void insertion(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > current) {
-                arr[j + 1] = arr[j];
-                j--;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break;
+                }
             }
-            arr[j + 1] = current;
         }
+    }
 
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
