0f4d946a27cc9e4f9b71dd1ac6049837bc96d290
shubham <shubham22121@gmail.com>
2022-01-20 22:23:59 +0530

    recurrsion

---
+++ 08-recursion/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n){
+
+        if(n == 5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+
+        print(n+1);
+    }
+}
