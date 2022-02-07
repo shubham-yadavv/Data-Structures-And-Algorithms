b323ce0b6e3342b318e8faa412d5b6f67d792913
shubham <shubham22121@gmail.com>
2022-02-08 02:25:06 +0530

    quick sort using recursion

--- 04-sorting/sorting/QuickSort.java
+++ 04-sorting/sorting/QuickSort.java
@@ -2,38 +2,46 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = {5,4,3,2,1};
         sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void sort(int[] nums, int low, int hi) {
-
-
-        int s = low;
-        int e = hi;
-        int m = s + (e - s) / 2;
-        int pivot = nums[m];
-
-        while (s <= e) {
-
-            while (nums[s] < pivot) {
-                s++;
-            }
-            while (nums[e] > pivot) {
-                e--;
-            }
-
-            if (s <= e) {
-                int temp = nums[s];
-                nums[s] = nums[e];
-                nums[e] = temp;
-                s++;
-                e--;
-            }
+    static void sort(int[] arr, int low, int high){
+        if (low >= high){
+            return;
         }
 
-        sort(nums, low, e);
-        sort(nums, s, hi);
+        int s = low;
+        int e = high;
+
+        int m = s + (e - s)/2;
+        int pivot = arr[m];
+
+        while (s<=e){
+
+            while (arr[s] < pivot){
+                s++;
+            }
+
+            while (arr[e] > pivot){
+                e--;
+            }
+
+            if (s<=e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+
+        }
+
+        sort(arr, low, e );
+        sort(arr, s, high);
+
+
     }
+
 }
--- 04-sorting/sorting/SelctionSort.java
+++ 04-sorting/sorting/SelctionSort.java
@@ -1,16 +1,28 @@
+import java.util.Arrays;
 
 public class SelctionSort {
-    public static void main(String[] args){
-        int[] arr = {5,4,3,2,1};
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 5};
+        selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
 
     }
 
-    static void selectionSort(int[] arr){
-        int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            int min = i;
-            for(int j=i+1; j<n; j++){
-
+    static void selection(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
             }
-            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selection(arr, r-1, 0, 0);
+        }
+    }
 }
