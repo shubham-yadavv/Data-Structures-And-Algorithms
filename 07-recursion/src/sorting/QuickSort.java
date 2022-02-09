64d8a30af778b7afb32803d77f9a5912fb6abe2e
shubham <shubham22121@gmail.com>
2022-02-10 02:41:55 +0530

    ouick sort using recursion

---
+++ 07-recursion/src/sorting/QuickSort.java
@@ -0,0 +1,47 @@
+package sorting;
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,3,5};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr, int low, int high){
+        if (low >= high){
+            return;
+        }
+
+        int s = low;
+        int e = high;
+
+        int m = s + (e - s)/2;
+        int pivot = arr[m];
+
+        while (s<=e){
+
+            while (arr[s] < pivot){
+                s++;
+            }
+
+            while (arr[e] > pivot){
+                e--;
+            }
+
+            if (s<=e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+
+        }
+
+        sort(arr, low, e );
+        sort(arr, s, high);
+
+    }
+
+}
