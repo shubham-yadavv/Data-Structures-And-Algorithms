05c77188411af70b458c316407e464d00dd4891b
shubham <shubham22121@gmail.com>
2022-02-09 16:47:05 +0530

    quick sort

--- 04-sorting/sorting/QuickSort.java
+++ 04-sorting/sorting/QuickSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {1,2,4,3,5};
         sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
