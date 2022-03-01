5b08a178a82cd101f00ea8a575318e52992a8b40
shubham <shubham22121@gmail.com>
2022-03-01 16:20:47 +0530

    ll

--- 09-Linked-List/DLL.java
+++ 09-Linked-List/DLL.java
@@ -86,11 +86,5 @@ public class DLL {
         public Node(int val) {
             this.val = val;
         }
-
-        public Node(int val, Node next, Node prev) {
-            this.val = val;
-            this.next = next;
-            this.prev = prev;
-        }
     }
 }
