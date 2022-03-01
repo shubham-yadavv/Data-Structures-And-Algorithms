c9f014eae386130d9d54b5e27f6b3b860fc865b5
shubham <shubham22121@gmail.com>
2022-03-02 01:18:17 +0530

    diameter O(n) of a tree

---
+++ 12-trees/binary-tree/DiameterOfTree2.java
@@ -0,0 +1,45 @@
+public class DiameterOfTree2 {
+
+    static class TreeInfo{
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam){
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static TreeInfo diameter(Node root) {
+        if(root == null){
+            return new TreeInfo(0, 0);
+        }
+
+        TreeInfo left = diameter(root.left);
+        TreeInfo right = diameter(root.right);
+
+        int myheight = Math.max(left.ht, right.ht) + 1;
+
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam = left.ht + right.ht + 1;
+
+        int mydiameter = Math.max(Math.max(diam1, diam2), diam);
+
+        return new TreeInfo(myheight, mydiameter);
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+
+        TreeInfo treeInfo = diameter(root);
+        System.out.println(treeInfo.diam);
+    }
+
+}
