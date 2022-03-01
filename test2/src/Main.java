fc5237337368b3d0f0279d1a46cba090a7a42a12
shubham <shubham22121@gmail.com>
2022-03-01 16:00:30 +0530

    update gitignore

--- test2/src/Main.java
+++ test2/src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        LL l = new LL();
-        l.insertFirst(7);
-        l.insertFirst(2);
-        l.insertFirst(8);
-        l.insertLast(100);
-        l.insertFirst(69);
-        l.display();
+        A a = new A();
+        a.foo();
+        
+    }
+}
+
+class A {
+    public void foo() {
+        System.out.println(" hello world");
     }
 }
