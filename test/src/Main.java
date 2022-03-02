a9754ba57adc1f91ee40f1a1421b53fde1b60f34
shubham <shubham22121@gmail.com>
2022-03-03 00:18:32 +0530

    graphs

---
+++ test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        LL l = new LL();
+        l.insertFirst(10);
+        l.insertFirst(12);
+        l.insertFirst(3);
+        l.insertFirst(4);
+        l.insertFirst(5);
+        l.insertLast(100);
+        l.display();
+    }
+}
