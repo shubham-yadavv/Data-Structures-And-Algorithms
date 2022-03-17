94a3292e0e7aba751b3c70921b1a0f826c57f2d6
shubham <shubham22121@gmail.com>
2022-03-17 21:02:05 +0530

    stack as linked list

--- 10-stack/stack/StackAsLinkedList.java
+++ 10-stack/stack/StackAsLinkedList.java
@@ -23,20 +23,15 @@ class StackUsingLinkedlist {
         node.data = x;
         node.next = top;
 
-        // update top reference
         top = node;
     }
-
-    // Utility function to check if the stack is empty or not
     public boolean isEmpty()
     {
         return top == null;
     }
 
-    // Utility function to return top element in a stack
     public int peek()
     {
-        // check for empty stack
         if (!isEmpty()) {
             return top.data;
         }
@@ -46,22 +41,18 @@ class StackUsingLinkedlist {
         }
     }
 
-    // Utility function to pop top element from the stack
-    public void pop() // remove at the beginning
+    public void pop()
     {
-        // check for stack underflow
         if (top == null) {
             System.out.print("\nStack Underflow");
             return;
         }
 
-        // update the top pointer to point to the next node
         top = (top).next;
     }
 
     public void display()
     {
-        // check for stack underflow
         if (top == null) {
             System.out.printf("\nStack Underflow");
 
@@ -70,41 +61,31 @@ class StackUsingLinkedlist {
             Node temp = top;
             while (temp != null) {
 
-                // print node data
-                System.out.printf("%d->", temp.data);
+                System.out.printf("->", temp.data);
 
-                // assign temp link to temp
                 temp = temp.next;
             }
         }
     }
 }
-// main class
 public class StackAsLinkedList {
     public static void main(String[] args)
     {
-        // create Object of Implementing class
         StackUsingLinkedlist obj = new StackUsingLinkedlist();
-        // insert Stack value
         obj.push(11);
         obj.push(22);
         obj.push(33);
         obj.push(44);
 
-        // print Stack elements
         obj.display();
 
-        // print Top element of Stack
         System.out.printf("\nTop element is %d\n", obj.peek());
 
-        // Delete top element of Stack
         obj.pop();
         obj.pop();
 
-        // print Stack elements
         obj.display();
 
-        // print Top element of Stack
         System.out.printf("\nTop element is %d\n", obj.peek());
     }
 }
