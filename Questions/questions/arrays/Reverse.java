6e8f4ecfdd47a4cfc85c31d5ecfd54b9de76f701
shubham <shubham22121@gmail.com>
2022-03-20 15:09:31 +0530

    reverse an array

--- Questions/questions/arrays/Reverse.java
+++ Questions/questions/arrays/Reverse.java
@@ -1,7 +1,19 @@
 import java.util.Arrays;
 
 public class Reverse {
+
     static void reverse(int[] arr, int start, int end){
+        int temp;
+        while (start < end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void recursiveReverse(int[] arr, int start, int end){
         int temp;
         if (start >= end){
             return;
@@ -9,14 +21,12 @@ public class Reverse {
         temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        reverse(arr, start+ 1, end-1);
+        recursiveReverse(arr, start+ 1, end-1);
     }
 
     static void print(int[] arr, int n){
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
-
-
         }
         System.out.println();
     }
@@ -24,10 +34,14 @@ public class Reverse {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         print(arr, arr.length);
-        reverse(arr, 0, arr.length-1);
 
-        System.out.println("reversed array is ");
+        System.out.println(" recursive reversed array is ");
+        recursiveReverse(arr, 0, arr.length-1);
         print(arr, arr.length);
+
+//        System.out.println("non recursive reversed array");
+//        reverse(arr, 0, arr.length-1);
+//        print(arr, arr.length);
 
     }
 }
