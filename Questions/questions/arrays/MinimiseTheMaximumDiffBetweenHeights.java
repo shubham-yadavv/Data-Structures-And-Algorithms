ec6ef4f5c5bb759e905380fb501d6b6ef6c39e1d
shubham <shubham22121@gmail.com>
2022-04-17 13:02:32 +0530

    min the max diff between heights

--- Questions/questions/arrays/MinimiseTheMaximumDiffBetweenHeights.java
+++ Questions/questions/arrays/MinimiseTheMaximumDiffBetweenHeights.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
-public class MinimiseTheMaximumDiffBetweenHeights {
 
+public class MinimiseTheMaximumDiffBetweenHeights {
 
   public static void main(String[] args) {
     int[] arr = {6,10};
