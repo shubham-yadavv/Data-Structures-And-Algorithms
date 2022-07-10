f2a506379890ef57b1c4c1b0fff92871af20d39f
shubham <shubham22121@gmail.com>
2022-07-10 14:26:42 +0530

    trapping water problem

--- Questions/questions/arrays/TrappingWaterProblem.java
+++ Questions/questions/arrays/TrappingWaterProblem.java
@@ -1,25 +1,30 @@
 public class TrappingWaterProblem {
 
-    static int trap(int[] arr, int n){
+    static int trap(int[] arr, int n) {
         int water = 0;
-        for (int i = 1; i < n - 1; i++) {
-            int left = arr[i];
-            for (int j = 0; j < i; j++) {
-                left = Math.max(left, arr[j]);
-            }
-            int right = arr[i];
-            for (int j = i + 1; j < n; j++) {
-                right = Math.max(right, arr[j]);
-            }
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-            water += Math.min(left, right) - arr[i] ;
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], arr[i]);
+        }
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            int min = Math.min(left[i], right[i]);
+            if (min > arr[i]) {
+                water += min - arr[i];
+            }
         }
         return water;
     }
 
     public static void main(String[] args) {
-        int[] arr = {3, 0, 2, 0, 4};
+        int[] arr = { 3, 0, 2, 0, 4 };
         int n = arr.length;
-        System.out.println(trap(arr,n));
+        System.out.println(trap(arr, n));
     }
 }
