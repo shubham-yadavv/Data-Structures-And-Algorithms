73ad868c55f8df50d2c13f2a4d53ee0ceaabaf1a
shubham <shubham22121@gmail.com>
2022-07-16 02:09:24 +0530

    questions

--- 02-linear-search/linear-search/Main.java
+++ 02-linear-search/linear-search/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int[] nums = { 23, 45, 1, 2, 8, 19, -3, 16, -11, 28 };
         int target = 19;
         int ans = linearSearch(nums, target);
-        boolean ans2 = linearSearch3(nums, target);
+        boolean ans2 = linearSearch2(nums, target);
 
         System.out.println(ans);
         System.out.println(ans2);
     }
 
     // search the target and return true or false
-    static boolean linearSearch3(int[] arr, int target) {
+    static boolean linearSearch2(int[] arr, int target) {
         if (arr.length == 0) {
             return false;
         }
@@ -24,34 +24,16 @@ public class Main {
         return false;
     }
 
-
-    static int linearSearch2(int[] arr, int target) {
-        if (arr.length == 0) {
-            return -1;
-        }
-
-        for (int index = 0; index < arr.length; index++) {
-            int element = arr[index];
-            if (element == target) {
-                return element;
-            }
-        }
-
-        return Integer.MAX_VALUE;
-    }
-
     static int linearSearch(int[] arr, int target) {
         if (arr.length == 0) {
             return -1;
         }
-
-        for (int index = 0; index < arr.length; index++) {
-            int element = arr[index];
-            if (element == target) {
-                return index;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
             }
-        }
 
+        }
         return -1;
     }
 
