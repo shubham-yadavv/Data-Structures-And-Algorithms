7d5ee7913b46546a69ce156d4bc772fa1bb2cc7f
shubham <shubham22121@gmail.com>
2022-07-18 10:11:06 +0530

    bs question

--- 03-binary search/binary-search/OrderAgnosticBS.java
+++ 03-binary search/binary-search/OrderAgnosticBS.java
@@ -1,6 +1,6 @@
 public class OrderAgnosticBS {
     public static void main(String[] args) {
-        int[] arr = {8,6,5,3,2,1};
+        int[] arr = { 8, 6, 5, 3, 2, 1 };
         int target = 2;
         int ans = orderAgnosticBS(arr, target);
         System.out.println(ans);
@@ -14,9 +14,10 @@ public class OrderAgnosticBS {
         // find whether the array is sorted in ascending or descending
         boolean isAsc = arr[start] < arr[end];
 
-        while(start <= end) {
+        while (start <= end) {
             // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds
+            // the range of int in java
             int mid = start + (end - start) / 2;
 
             if (arr[mid] == target) {
---
+++ Questions/questions/BinarySearch/PeakIndexOfMountain.java
@@ -0,0 +1,28 @@
+import javax.management.StandardEmitterMBean;
+
+public class PeakIndexOfMountain {
+
+    public static int peakindex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5, 3, 2 };
+        System.out.println(peakindex(arr));
+
+    }
+
+}
