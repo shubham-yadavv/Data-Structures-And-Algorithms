8d3ac8f199d2789720e71b4c3737729c04532c56
shubham <shubham22121@gmail.com>
2022-08-05 19:36:59 +0530

    sorting

--- 04-sorting/BubbleSort.java
+++ 04-sorting/BubbleSort.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = { 5, 4, 3, 2, 1 };
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-   static void bubble(int[] arr){
+    static void bubble(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -17,5 +17,5 @@ public class BubbleSort {
                 }
             }
         }
-   }
+    }
 }
--- Questions/450 questions/arrays/KadanesAlgo.java
+++ Questions/450 questions/arrays/KadanesAlgo.java
@@ -3,7 +3,7 @@
 // find contagious sub array with max sum
 
 public class KadanesAlgo {
-    long maxSubarraySum(int[] arr, int n) {
+    static long maxSubarraySum(int[] arr) {
         int max = arr[0], sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
@@ -16,5 +16,10 @@ public class KadanesAlgo {
             }
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, -2, 5 };
+        System.out.println(maxSubarraySum(arr));
     }
 }
