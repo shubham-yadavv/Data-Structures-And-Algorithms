0ec27e7d69c58cac565e838b937c46dfce14bc2d
Harshit Godawat <81518724+Vector717@users.noreply.github.com>
2022-10-21 01:34:27 +0530

    Update Ceiling.java

--- Questions/questions/BinarySearch/Ceiling.java
+++ Questions/questions/BinarySearch/Ceiling.java
@@ -3,7 +3,7 @@ public class Ceiling {
 
     public static void main(String[] args) {
         int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
-        int target = 15;
+        int target = 20;
         int ans = ceiling(arr, target);
         System.out.println(ans);
 
